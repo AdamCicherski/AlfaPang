d3b8f1d194d5385ae308367dd1e6f67251a98add
AdamCicherski <adam.cicherski.97@gmail.com>
2025-03-08 10:56:37 +0000

    Modify bfs procedure to use new reversed index and remove visited_edges table

--- include/bfs.hpp
+++ include/bfs.hpp
@@ -1,51 +1,74 @@
 #ifndef BFS_IMPL_HPP
 #define BFS_IMPL_HPP
 
-#include "../externals/robin-hood-hashing/src/include/robin_hood.h"
-#include "bfs.h"
+// #include "bfs.h"
 #include <queue>
 #include <vector>
 
+// Norbert's trick + one vector
 template <typename T1, typename T2>
-void find_connected_bfs(const std::vector<T1> &data,
+void find_connected_bfs(const std::vector<T1> &kmers_vec,
                         const std::string &sequence,
-                        const std::vector<std::vector<T1>> &kmers, int k,
-                        std::vector<T2> &visited, T2 &v) {
+                        const std::vector<T1> &reversed_index,
+                        const std::vector<T1> &kmers_occ, int k,
+                        std::vector<T2> &visited_nodes, T2 &v) {
   // needs a variable for bits shift depending on template
+  int shift = sizeof(T1) * 8 - 1;
   for (T1 j = 0; j < sequence.size(); j++) {
-    if (visited[j] == 0 && sequence[j] != '$') {
-      auto seen_kmers = robin_hood::unordered_set<robin_hood::pair<T1, int>>{};
-      std::vector<T1> queue = {j};
-      while (!queue.empty()) {
-        auto pos = queue.back();
-        queue.pop_back();
-        for (int i = 0; i < k; i++) {
-          if (pos - i >= 0) [[likely]] {
-            auto kmer_index = data[pos - i];
-            int c = (kmer_index < 0) ? (k - 1 - i) : i;
-            robin_hood::pair<T1, int> myVar(abs(kmer_index), c);
-            if (seen_kmers.find(myVar) != seen_kmers.end()) {
-              continue;
-            }
-            seen_kmers.insert(myVar);
-            for (auto map : kmers[abs(kmer_index)]) {
+    if (visited_nodes[j] != 0 || sequence[j] == '$') {
+      continue;
+    }
+    std::queue<T1> queue;
+    queue.push(j);
+    visited_nodes[j] = v; // potrzebne dla filtra
+    while (!queue.empty()) {
+      auto pos = queue.front();
+      queue.pop();
+      for (int i = 0; i < k; i++) {
+        if (pos - i >= 0) [[likely]] {
+          auto kmer = kmers_vec[pos - i];
+          if (kmer == 0) {
+            continue;
+          }
+
+          auto kmer_index = abs(kmer);
+          auto c = reversed_index[kmers_occ[kmer_index - 1]];
+          auto new_edge =
+              c + ((kmer ^ kmers_vec[c]) < 0) * (k - 1) +
+              (1 | (kmer >> shift)) * (1 | (kmers_vec[c] >> shift)) * i;
+          if (pos == new_edge) {
+            for (size_t occ = kmers_occ[kmer_index - 1];
+                 occ < kmers_occ[kmer_index]; occ++) {
+              auto map = reversed_index[occ];
+
               auto new_pos =
-                  map + ((kmer_index ^ data[map]) < 0) * (k - 1) +
-                  (1 | (kmer_index >> 31)) * (1 | (data[map] >> 31)) * i;
-              if (visited[new_pos] != 0) {
+                  map + ((kmer ^ kmers_vec[map]) < 0) * (k - 1) +
+                  (1 | (kmer >> shift)) * (1 | (kmers_vec[map] >> shift)) * i;
+              if (visited_nodes[new_pos] != 0) {
                 continue;
               } else {
-                visited[new_pos] = (sequence[new_pos] == sequence[j]) ? v : -v;
-                queue.push_back(new_pos);
+                visited_nodes[new_pos] =
+                    (sequence[new_pos] == sequence[j]) * v +
+                    (sequence[new_pos] != sequence[j]) * (-v);
+                queue.push(new_pos);
               }
             }
-          } else [[unlikely]] {
-            break;
+          } else {
+            if (visited_nodes[new_edge]) {
+              continue;
+            }
+            queue.push(new_edge);
+            visited_nodes[new_edge] =
+                (sequence[new_edge] == sequence[j]) * v +
+                (sequence[new_edge] != sequence[j]) * (-v);
           }
+        } else [[unlikely]] {
+          break;
         }
       }
-      v++;
     }
+    v++;
   }
 }
+
 #endif // BFS_IMPL_HPP
